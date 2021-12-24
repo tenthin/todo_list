fe1d3eb3218e388ece4eff1d5321290b808c8ab1
Tenzin <tenthinten@gmail.com>
2021-12-24 15:06:38 +0530

    create a form space to update the task

--- tasks/views.py
+++ tasks/views.py
@@ -20,3 +20,12 @@ def index(request):
 
     context = {'tasks':tasks,'form':form}
     return render(request,'tasks/list.html',context)
+
+def updateTask(request,pk):
+    task = Task.objects.get(id=pk)  
+
+    form = TaskForm(instance=task)
+
+    context = {'form':form} 
+
+    return render(request, 'tasks/update_task.html',context)
