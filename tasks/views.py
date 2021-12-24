56fc165a1fc0afb6860ea69fac518a585741320e
Tenzin <tenthinten@gmail.com>
2021-12-24 14:34:27 +0530

    now we can create/list items

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 from .models import *
@@ -10,6 +10,13 @@ def index(request):
     tasks=Task.objects.all()
 
     form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
 
     context = {'tasks':tasks,'form':form}
     return render(request,'tasks/list.html',context)
